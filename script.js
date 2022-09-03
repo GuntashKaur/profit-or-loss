3a782d84d1f2d225c0317870f24c57d9ac2ce52c
Guntash Kaur <83450329+GuntashKaur@users.noreply.github.com>
2022-09-03 15:28:34 +0530

    updated

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
-var inputPrice = document.querySelector("#input-price");
+var initialtPrice = document.querySelector("#input-price");
 var quantityOfStock = document.querySelector("#quantity-of-stock");
 var currentPrice = document.querySelector("#current-price");
 var button = document.querySelector("#submit-btn");
 var outputBox = document.querySelector("#outputEl");
 
+function calculate(initial, quantity, current){
+    if(initial > current){
+        //Loss
+    }
+    else if(current > initial ){
+        //gain
+    }
+    else{
+        console.log(`no pain, no gain and no gain, no pain`)
+    }
+}
+
+calculate(10, 10, 10)
