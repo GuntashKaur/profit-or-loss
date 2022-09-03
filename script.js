12cadba5d43c4f06df6d22cad06ac399e9155adf
Guntash Kaur <83450329+GuntashKaur@users.noreply.github.com>
2022-09-03 14:26:16 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,6 @@
+var inputPrice = document.querySelector("#input-price");
+var quantityOfStock = document.querySelector("#quantity-of-stock");
+var currentPrice = document.querySelector("#current-price");
+var button = document.querySelector("#submit-btn");
+var outputBox = document.querySelector("#outputEl");
+
