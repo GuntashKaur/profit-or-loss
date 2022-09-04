848d72775118f973e3085bf499a2496c83d35e1f
Guntash Kaur <83450329+GuntashKaur@users.noreply.github.com>
2022-09-04 16:20:55 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,41 @@
-var initialtPrice = document.querySelector("#input-price");
+var initialPrice = document.querySelector("#input-price");
 var quantityOfStock = document.querySelector("#quantity-of-stock");
 var currentPrice = document.querySelector("#current-price");
 var button = document.querySelector("#submit-btn");
 var outputBox = document.querySelector("#outputEl");
 
+
+button.addEventListener("click", clickHandler);
+
+function clickHandler(){
+
+    var inp = Number(initialPrice.value);
+    var quan = Number(quantityOfStock.value);
+    var curr = Number(currentPrice.value);
+
+    calculate(inp, quan, curr);
+}
+
 function calculate(initial, quantity, current){
     if(initial > current){
-        //Loss
+        var loss = (initial - current) * quantity;
+        var lossPercentage = (loss / initial) * 100;
+
+        showOutput(`The loss is ${loss} and percentage is ${lossPercentage}%`)
     }
     else if(current > initial ){
-        //gain
+        var gain = (current - initial) * quantity;
+        var gainPercentage = (gain / initial) * 100;
+
+        showOutput(`The profit is ${gain} and percentage is ${gainPercentage}`);
     }
     else{
         console.log(`no pain, no gain and no gain, no pain`)
     }
 }
 
-calculate(10, 10, 10)
+
+
+function showOutput(message){
+    outputBox.innerHTML = message;
+}
